7579b209ad4c63874bb4d0b089e9a80b50e82cf1
haminhdat <dat.ha@vexere.com>
2018-07-20 21:27:23 +0700

    add connection api deploy

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const config = {
   PORT_FRONTEND: 3001,
-  BASE_API_URL: "http://localhost:3000/api",
+  BASE_API_URL: "https://reactjs-template-be.herokuapp.com/api",
   // ALLOWEDED_HOSTS: ["onehealth.localhost", "user.localhost"],
   BASE_DOMAIN: "localhost",
   PRODUCTION_ENV: process.env.NODE_ENV === "production"
